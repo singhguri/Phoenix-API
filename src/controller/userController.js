505ecc2aa756099e4bd37e9bdad2fbe36eba1390
singhguri <singhguri5598@gmail.com>
2023-02-24 12:15:02 -0500

    added logs

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -61,7 +61,7 @@ const loginByOAuth = async (req, res) => {
         .send({ status: true, message: "User first log in successful." });
     }
   } catch (error) {
-    console.log({ error });
+    console.log(req.body);
     res.status(500).send({ status: false, message: error.message });
   }
 };
